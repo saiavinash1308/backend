c0016f77eb4042f71582940ad3d1447b8e60dc26
MA-2130 <ma-2130@PC-105s-MacBook-Pro.local>
2025-01-07 16:18:00 +0530

    change

--- apps/ws/src/manager/game/GameManager.ts
+++ apps/ws/src/manager/game/GameManager.ts
@@ -27,36 +27,65 @@ export class GameManager{
         const game = this.games.get(roomId);
         if(!game) return;
         const room = appManager.getRoom(roomId);
-        if(!room || room.getGameType() !== "LUDO")  return;
-        const ludogame = game as LudoGame
-        ludogame.rollDice(playerId)
+        if(!room) return;
+        if(room.getGameType() === "LUDO"){
+            const ludogame = game as LudoGame
+            ludogame.rollDice(playerId)
+        }
+        else if(room.getGameType() === "FAST_LUDO"){
+            const fastludogame = game as FastLudoGame
+            fastludogame.rollDice(playerId)
+        }
+        return
+        
     }
 
     fetchLudoGameAndMovePiece(roomId: string, playerId: string, piece: number){
         const game = this.games.get(roomId);
         if(!game) return;
         const room = appManager.getRoom(roomId);
-        if(!room || room.getGameType() !== "LUDO")  return;
-        const ludogame = game as LudoGame
-        ludogame.makeMove(playerId, piece);
+        if(!room) return;
+        if(room.getGameType() === "LUDO"){
+            const ludogame = game as LudoGame
+            ludogame.makeMove(playerId, piece)
+        }
+        else if(room.getGameType() === "FAST_LUDO"){
+            const fastludogame = game as FastLudoGame
+            fastludogame.makeMove(playerId, piece)
+        }
+        return
     }
 
     fetchLudoGameAndUpdateMove(roomId: string, playerId: string){
         const game = this.games.get(roomId);
         if(!game) return;
         const room = appManager.getRoom(roomId);
-        if(!room || room.getGameType() !== "LUDO")  return;
-        const ludogame = game as LudoGame
-        ludogame.moveUpdate(playerId);
+        if(!room) return;
+        if(room.getGameType() === "LUDO"){
+            const ludogame = game as LudoGame
+            ludogame.moveUpdate(playerId)
+        }
+        else if(room.getGameType() === "FAST_LUDO"){
+            const fastludogame = game as FastLudoGame
+            fastludogame.moveUpdate(playerId)
+        }
+        return
     }
 
     fetchLudoGameAndUpdateTurn(roomId: string, playerId: string){
         const game = this.games.get(roomId);
         if(!game) return;
         const room = appManager.getRoom(roomId);
-        if(!room || room.getGameType() !== "LUDO")  return;
-        const ludogame = game as LudoGame
-        ludogame.forceUpdateTurn(playerId);
+        if(!room) return;
+        if(room.getGameType() === "LUDO"){
+            const ludogame = game as LudoGame
+            ludogame.forceUpdateTurn(playerId)
+        }
+        else if(room.getGameType() === "FAST_LUDO"){
+            const fastludogame = game as FastLudoGame
+            fastludogame.forceUpdateTurn(playerId)
+        }
+        return
     }
 
     fetchFastLudoGameAndEndGame(roomId: string){
