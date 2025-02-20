be9d81387fc3077cd1213a9a5050b377f19c8b0f
Sai Avinash <130341832+saiavinash1308@users.noreply.github.com>
2025-02-20 14:32:29 +0530

    change

--- apps/ws/src/manager/game/MemoryGame.ts
+++ apps/ws/src/manager/game/MemoryGame.ts
@@ -63,7 +63,10 @@ export class MemoryGame{
 
         private handleTurn(){
             this.currentPlayer = this.currentPlayer === this.player1 ? this.player2 : this.player1;
-            return this.currentPlayer;
+            setTimeout(() => {
+                socketManager.broadcastToRoom(this.roomId, "MEMORY_GAME_CURRENT_TURN", this.currentPlayer)
+            }, 500);
+            
         }
 
         private isValidTurn(playerId: string){
@@ -109,16 +112,13 @@ export class MemoryGame{
             }
             else{
                 setTimeout(() => {
-                    const currentTurn = this.handleTurn();
                     const message = JSON.stringify({index1: this.card1Index, index2: index})
                     setTimeout(() => {
                         console.log("Close cards")
                         socketManager.broadcastToRoom(this.roomId, "CLOSE_CARDS", message)
                         this.card1Index = -1
                         this.card1 = null
-                        setTimeout(() => {
-                            socketManager.broadcastToRoom(this.roomId, "MEMORY_GAME_CURRENT_TURN", currentTurn)
-                        }, 500);
+                        this.handleTurn();
                     }, 1000);
                 }, 1000);
                 
